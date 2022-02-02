56c4e63ceb0b7e6d59b2dc0e2de096edb2c1d2b0
ToastyNuggetCode <95596702+ToastyNuggetCode@users.noreply.github.com>
2022-02-01 19:52:48 -0500

    New code for E Clicker that has not been released on Itch.io
    This is the new code for E Clicker that will be released soon on Itch.io. There is still more code that is coming but this is what I have for now.

--- app.py
+++ app.py
@@ -4,12 +4,36 @@ from tkinter import ttk
 
 count = 0
 
+def reset():
+    global count
+    
+
+    count = 0
+
+    label1.configure(text=f' {count} E s')
+
 def clicked(): # without event because I use `command=` instead of `bind`
     global count
 
     count = count + 1
 
     label1.configure(text=f' {count} E s ')
+
+
+
+
+    if count == 21:
+
+        label= tk.Label(windows, text="Advancment: haha meme go brrrrrr (get to 21 E s")
+        label.grid(column=0, row=2)
+
+    if count == 100:
+        label= tk.Label(windows, text="Advancment: a master upon E s (get to 100 E s")
+        label.grid(column=0, row=3)
+
+    if count == 1000:
+        label= tk.Label(windows, text="Advancment: wow you actually made it this far? (get to 1000 E s")
+        label.grid(column=0, row=4)
 
 
 windows = tk.Tk()
@@ -21,7 +45,10 @@ label.grid(column=0, row=0)
 label1 = tk.Label(windows)
 label1.grid(column=0, row=1)
 
-custom_button = ttk.Button(windows, text="Add 1 E", command=clicked)
-custom_button.grid(column=1, row=0)
+plusE = ttk.Button(windows, text="Add 1 E", command=clicked)
+plusE.grid(column=1, row=0)
+
+resetbutt = ttk.Button(windows, text="Reset Counter", command=reset)
+resetbutt.grid(column=1, row=1)
 
 windows.mainloop()
